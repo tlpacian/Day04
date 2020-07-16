7e484a81f45f5e533751b355d487b3a0d6307b91
root <root@DESKTOP-OB7DKIA.localdomain>
2020-07-16 21:45:29 +0200

    iteractive power

---
+++ ex02/ft_iteractive_power.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int ft_iteractive_power(int nb, int power)
+{
+	int i;
+	int count;
+
+	i = 0;
+	count = 1;
+	
+	if (power < 0)
+		return (0);
+	while (i < power)
+	{
+	  count *nb = count;
+	  return (count);
+	}
+}
